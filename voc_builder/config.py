bf9201064b6b02e3525753ee1e15c84e85b0aa46
mpwang <randall.wjz@gmail.com>
2023-03-13 02:00:10 +0800

    enable customized default location

--- voc_builder/config.py
+++ voc_builder/config.py
@@ -1,9 +1,11 @@
 """Store configuration for project"""
+import os
 from pathlib import Path
 
 # Testings should patch these variables
 #
 # The default path for storing the vocabulary book
-DEFAULT_CSV_FILE_PATH = Path('~/aivoc_builder.csv').expanduser()
+aivoc_home = os.environ.get("AIVOC_HOME", "~")
+DEFAULT_CSV_FILE_PATH = Path(f"{aivoc_home}/aivoc_builder.csv").expanduser()
 # The default path for storing db files
-DEFAULT_DB_PATH = Path('~/.aivoc_db').expanduser()
+DEFAULT_DB_PATH = Path(f"{aivoc_home}/.aivoc_db").expanduser()
