4c8937a6a88ae94387fa435b97c1eb2316c15339
Randall Wang <randall.wjz@gmail.com>
2023-03-13 11:10:14 +0800

    use AIVOC_DATA_DIR to specify data directory

--- voc_builder/config.py
+++ voc_builder/config.py
@@ -5,7 +5,10 @@ from pathlib import Path
 # Testings should patch these variables
 #
 # The default path for storing the vocabulary book
-aivoc_home = os.environ.get("AIVOC_HOME", "~")
-DEFAULT_CSV_FILE_PATH = Path(f"{aivoc_home}/aivoc_builder.csv").expanduser()
+data_dir = os.environ.get('AIVOC_DATA_DIR', '~')
+if data_dir.endswith('/'):
+    data_dir = data_dir[:-1]
+
+DEFAULT_CSV_FILE_PATH = Path(f'{data_dir}/aivoc_builder.csv').expanduser()
 # The default path for storing db files
-DEFAULT_DB_PATH = Path(f"{aivoc_home}/.aivoc_db").expanduser()
+DEFAULT_DB_PATH = Path(f'{data_dir}/.aivoc_db').expanduser()
